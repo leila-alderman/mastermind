7f29986c19901d5df5045b48267b74cb0f4cdd6c
leila-alderman <leila.alderman@gmail.com>
2019-01-15 15:00:14 -0500

    Add basic functionality

--- mastermind.rb
+++ mastermind.rb
@@ -4,19 +4,23 @@ class Game
     end
 
     def create_code
-        nums = [0..9]
+        nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         nums.sample(4)
     end
 
     def round(turn)
         puts "Enter your guess:"
-        guess = gets.chomp.split(" ")
-        return if guess == 'q'
-        check(guess)
+        guess = gets.chomp
+        return -1 if guess == 'q'
+        guess = guess.split(" ").map(&:to_i)
+        result = check(guess)
+        return -1 if result < 0
         puts "Sorry -- you're out of guesses. Game over." if turn == 12
+        return 1
     end
 
     def check(guess)
+        puts "The guess is #{guess}."
         right_position = 0
         right_value = 0
         for i in 0..(guess.length-1)
@@ -26,15 +30,22 @@ class Game
             if @code.include?(guess[i])
                 right_value += 1
             end
+            puts "Loop number #{i}"
+            puts "Right position = #{right_position}"
+            puts "Right value = #{right_value}"
         end
+        puts "After the loop, position = #{right_position}"
+        puts "After the loop, value = #{right_value}"
         right_value -= right_position
         puts "Your guess had #{right_position} #{pluralize(right_position, "number")} with the correct value in the correct position and #{right_value} #{pluralize(right_value, "number")} with the correct value but in the wrong position."
-        winner?(right_position)
+        return -1 if winner?(right_position)
+        return 1
     end
 
     def winner?(number)
         if number == 4
             puts "Congrats! You win!"
+            return true
         end
     end
 
@@ -44,8 +55,13 @@ class Game
         puts "You have 12 rounds to try to guess the correct digits in the correct order."
         puts "Enter your guesses in the format '1 2 3 4'."
         puts "To quit the game, enter 'q'."
+        puts "The answer is #{@code}"
         for turn in 1..12
-            round(turn)
+            result = round(turn)
+            if result < 0
+                puts "Thanks for playing!"
+                return
+            end
         end
 
     end
